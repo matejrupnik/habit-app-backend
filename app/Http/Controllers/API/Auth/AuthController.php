5974bbec07b94b434b9035a0968b4fecbb9005a2
matej <matej.rupnik02@gmail.com>
2023-01-03 19:20:43 +0100

    admin

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -40,10 +40,10 @@ class AuthController extends Controller
     public function register(Request $request) {
 
         $fields = $request->validate([
-            "username" => "required|string|unique:users,username",
+//            "username" => "required|string|unique:users,username",
 //            "first_name" => "required|string",
 //            "middle_name" => "string",
-//            "last_name" => "string",
+            "last_name" => "string",
 //            "email" => "required|email:rfc,dns|string|unique:users,email",
 //            "password" => "required|string|confirmed"
         ]);
